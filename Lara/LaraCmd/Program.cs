7bbeb9325ccd3a99768b3d68e60f7e5628bff9f7
aloisdg <aloisdegouvello@live.fr>
2014-04-04 09:58:17 +0200

    Added Command Line Support

---
+++ Lara/LaraCmd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaraCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lara.Core lara = new Lara.Core();
+            lara.GetAllText(@"C:\Users\alois\Documents\GitHub\Lara\Lara\LaraCmd\", "cs", false);
+            foreach (var item in lara.Words)
+            {
+                Console.WriteLine(String.Format("{0}\t{1}", item.Value.ToString(), item.Key.ToString()));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lara/LaraCore/Core.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lara
+{
+    public class Core
+    {
+        public Dictionary<string, uint> Words;
+     
+        public Core()
+        {
+            Words = new Dictionary<string, uint>();
+        }
+
+        public void GetAllText(string folderPath, string format, bool isRecursive)
+        {
+            foreach (string file in Directory.EnumerateFiles(folderPath,
+                String.IsNullOrWhiteSpace(format) ? "*" : String.Format("*.{0}", format),
+                isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly))
+            {
+                string[] wordtab = SplitWords(File.ReadAllText(file));
+                foreach (var item in wordtab)
+                {
+                    if (Words.ContainsKey(item))
+                    {
+                        Words[item]++;
+                    }
+                    else
+                    {
+                        Words.Add(item, 1);
+                    }
+                }
+            }
+            var ordered = Words.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            Words = ordered;
+        }
+
+
+        private string[] SplitWords(string s)
+        {
+            //
+            // Split on all non-word characters.
+            // ... Returns an array of all the words.
+            //
+            return Regex.Split(s, @"\W+");
+            // @      special verbatim string syntax
+            // \W+    one or more non-word characters together
+        }
+    }
+}
