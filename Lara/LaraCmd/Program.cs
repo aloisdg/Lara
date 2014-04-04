fda51e9241561863d9d3fb8ae2f0ae7360ecdf4b
aloisdg <aloisdegouvello@live.fr>
2014-04-04 12:45:22 +0200

    Added more example and improved output

--- Lara/LaraCmd/Program.cs
+++ Lara/LaraCmd/Program.cs
@@ -12,11 +12,14 @@ namespace LaraCmd
         {
             Lara.Core lara = new Lara.Core();
             lara.GetAllText(@"C:\Users\alois\Downloads\DotNetReferenceSource\Source", "cs", System.IO.SearchOption.AllDirectories, true);
-
             for (int i = 0; i < lara.Words.Count && i < 10; i++)
             {
-                Console.WriteLine(String.Format("{0}\t{1}", lara.Words.ElementAt(i).Value.ToString(), lara.Words.ElementAt(i).Key.ToString()));
+                Console.WriteLine(String.Format("{0}\t{1}\t{2}",
+                    lara.Words.ElementAt(i).Value.ToString(),
+                    lara.Words.ElementAt(i).Key.ToString(),
+                    (lara.Words.ElementAt(i).Value / lara.GetWordsTotal() * 100).ToString("0.00") + " %"));
             }
+            Console.WriteLine(lara.Words.Count);
             Console.ReadLine();
         }
     }
