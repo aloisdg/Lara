f927b861dea316cfccd0ac98676ff13a9679c4ec
aloisdg <aloisdegouvello@live.fr>
2014-04-04 18:43:28 +0200

    Improved output

--- Lara/LaraCmd/Program.cs
+++ Lara/LaraCmd/Program.cs
@@ -12,14 +12,21 @@ namespace LaraCmd
         {
             Lara.Core lara = new Lara.Core();
             lara.GetAllText(@"C:\Users\alois\Downloads\DotNetReferenceSource\Source", "cs", System.IO.SearchOption.AllDirectories, true);
+            //lara.GetAllText(@"C:\Users\alois\Documents\GitHub\Lara\Lara\LaraCmd\", "cs", System.IO.SearchOption.AllDirectories, true);
+            
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("=========== Power by Lara ===========");
+            Console.WriteLine("=========== Made by Alois ===========");
+            Console.WriteLine(Environment.NewLine);
             for (int i = 0; i < lara.Words.Count && i < 10; i++)
             {
-                Console.WriteLine(String.Format("{0}\t{1}\t{2}",
+                Console.WriteLine(String.Format("{0,-8}\t{1,-8}\t{2,4}",
                     lara.Words.ElementAt(i).Value.ToString(),
                     lara.Words.ElementAt(i).Key.ToString(),
-                    (lara.Words.ElementAt(i).Value / lara.GetWordsTotal() * 100).ToString("0.00") + " %"));
+                    (lara.Words.ElementAt(i).Value / lara.GetWordsTotal() * 100).ToString("0.00") + "%"));
             }
-            Console.WriteLine(lara.Words.Count);
+            Console.WriteLine(String.Format("Count{0,32}\nTotal{1,32}\r\n", lara.Words.Count, lara.GetWordsTotal()));
+            Console.WriteLine("=====================================");
             Console.ReadLine();
         }
     }
